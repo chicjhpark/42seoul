7d5bcdd802a97969ef70dbcb25e745faacbd590c
chicjhpark <chicjhpark@gmail.com>
2021-08-30 23:26:28 +0900

    mandatory

--- pipex/srcs_bonus/pipex_bonus.c
+++ pipex/srcs_bonus/pipex_bonus.c
@@ -6,11 +6,28 @@
 /*   By: jaehpark <jaehpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 09:44:18 by jaehpark          #+#    #+#             */
-/*   Updated: 2021/08/30 12:27:20 by jaehpark         ###   ########.fr       */
+/*   Updated: 2021/08/30 23:09:30 by jaehpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+int	parse_file(int argc, char **argv)
+{
+	int	infile;
+	int	outfile;
+
+	if (access(argv[1], F_OK | R_OK) == -1)
+		error_msg("exist or read");
+	infile = open(argv[1], O_RDONLY);
+	if (infile < 0)
+		error_msg("open");
+	dup2(infile, STDIN_FILENO);
+	outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (outfile < 0)
+		error_msg("open");
+	return (outfile);
+}
 
 void	exe_cmd(char *argv, char **envp)
 {
@@ -31,29 +48,12 @@ void	exe_cmd(char *argv, char **envp)
 	}
 	else if (pid > 0)
 	{
-		wait(0);
+		waitpid(pid, 0, WNOHANG);
 		close(fd[1]);
 		dup2(fd[0], STDIN_FILENO);
 	}
 	else
 		error_msg("fork");
-}
-
-int	parse_file(int argc, char **argv)
-{
-	int	infile;
-	int	outfile;
-
-	if (access(argv[1], F_OK | R_OK) == -1)
-		error_msg("exist or read");
-	infile = open(argv[1], O_RDONLY);
-	if (infile < 0)
-		error_msg("open");
-	dup2(infile, STDIN_FILENO);
-	outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (outfile < 0)
-		error_msg("open");
-	return (outfile);
 }
 
 int	main(int argc, char **argv, char **envp)
